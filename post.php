216d516a0abc332e524fa358a94e676e38639982
wattanawipa <minn7714@gmail.com>
2024-01-05 11:11:22 +0700

    commit to php1
    commit to php1

---
+++ post.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard wtnwp</h1>
+    <hr>
+    <div style="text-align: center;">
+    <form action="id" method="get">
+    <p style="text-align: center;"></p>
+    <?php
+        echo "ต้องการดูกระทู้หมายเลข $_GET[id] <br>"
+        ?>
+    <table style="border: 2px solid black; width: 40%;" align="center">
+        <tr><td colspan="2" style="background-color: #6cd2fe;">แสดงความคิดเห็น</td></tr>
+        <tr><td align="center">
+                <form>
+                    <textarea name="message" rows="10" cols="30"></textarea>
+                </form>
+            </td></tr>
+        <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+    </form>
+    </div>
+</table>
+<center><a href="index.html">กลับไปหน้าหลัก</a></center> 
+</body>
+</html>
---
+++ verify.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>verify</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard wtnwp</h1>
+    <hr>
+    <div style="text-align: center;">
+        เข้าสู่ระบบด้วย <br>
+        login = <?php echo $_POST['login']; ?> <br>
+        Password = <?php echo $_POST['password']; ?> <br>
+</div>
+</body>
+</html>
