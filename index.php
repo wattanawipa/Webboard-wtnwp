e3903a57b13bbee73581e7929f071865a51f0d16
wattanawipa <minn7714@gmail.com>
2024-02-09 12:12:54 +0700

    commit
    commit

--- index.php
+++ index.php
@@ -6,11 +6,36 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Webboard</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard wtnwp</h1>
+    <div class="container-lg">
+    <h1 style="text-align: center;" class="mt-3">Webboard wtnwp</h1>
     <hr>
+    <nav class="navbar navbar-expand-lg" style="background-color: #d3d3d3;">
+        <div class="container-lg">
+            <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
+      <ul class="navbar-nav">
+        <?php if(!isset($_SESSION['id'])){ ?>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
+        </li>
+        <?php }else{ ?>
+            <li class="nav-item dropdown">
+          <a class="btn btn-outline-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="bi bi-person-lines-fill"> </i><?php echo $_SESSION['username']; ?>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
+          </ul>
+        </li>
+        <?php } ?>
+      </ul>
+  </div>
+</nav>
     <form>
     หมวดหมู่: <select name "category">
          <option value="all">--ทั้งหมด--</option>
@@ -32,6 +57,7 @@ session_start();
 
     </form>
     <br>
+    <ul>
     <?php
     for($i = 1; $i<=10; $i++){
         echo "<li><a href=post.php?id=$i>กระทู้ที่$i</a>";
@@ -42,5 +68,7 @@ session_start();
     }
     ?>
 
+    </ul>
+</div>
 </body>
 </html>
--- verify.php
+++ verify.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>verify</title>
+    <title>Webboard</title>
 </head>
 <body>
     <h1 style="text-align: center;">Webboard wtnwp</h1>
