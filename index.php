1ea3ef086e6414b793277f0c4449621e7acf9cb5
wattanawipa <minn7714@gmail.com>
2024-02-23 10:52:32 +0700

    work7 Finished(bootstrap)
    work7 Finished(bootstrap)

--- index.php
+++ index.php
@@ -15,27 +15,7 @@ session_start();
     <div class="container-lg">
     <h1 style="text-align: center;" class="mt-3">Webboard wtnwp</h1>
     <hr>
-    <nav class="navbar navbar-expand-lg" style="background-color: #d3d3d3;">
-        <div class="container-lg">
-            <a class="navbar-brand" href="index.php"><i class="bi bi-house-door-fill"></i> Home</a>
-      <ul class="navbar-nav">
-        <?php if(!isset($_SESSION['id'])){ ?>
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="login.php"><i class="bi bi-pencil-square"></i> เข้าสู่ระบบ</a>
-        </li>
-        <?php }else{ ?>
-            <li class="nav-item dropdown">
-          <a class="btn btn-outline-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          <i class="bi bi-person-lines-fill"> </i><?php echo $_SESSION['username']; ?>
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="logout.php"><i class="bi bi-power"></i> ออกจากระบบ</a></li>
-          </ul>
-        </li>
-        <?php } ?>
-      </ul>
-  </div>
-</nav>
+    <?php include "nav.php" ?>
           <div class="mt-3 d-flex justify-content-between">
             <div>
               <label>หมวดหมู่</label>
--- verify.php
+++ verify.php
@@ -13,8 +13,7 @@ if(isset($_SESSION['id'])){
     <title>Webboard</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard wtnwp</h1>
-    <hr>
+
     <div style="text-align: center;">
 
        <?php
@@ -24,21 +23,28 @@ if(isset($_SESSION['id'])){
             $_SESSION['username']='admin';
             $_SESSION['role']='a';
             $_SESSION['id']=session_id();
-            echo "ยินดีต้อนรับคุณ ADMIN <br>";
-            echo "<a href=index.php>กลับไปหน้าหลัก</a>";
+            header("location:index.php");
+            die();
+           // echo "ยินดีต้อนรับคุณ ADMIN <br>";
+            //echo "<a href=index.php>กลับไปหน้าหลัก</a>";
         }
         elseif($login=="member" && $pwd=="mem1234")
         {
             $_SESSION['username']='member';
             $_SESSION['role']='m';
             $_SESSION['id']=session_id();
-            echo "ยินดีต้อนรับคุณ MEMBER <br>";
-            echo "<a href=index.php>กลับไปหน้าหลัก</a>";
+            header("location:index.php");
+            die();
+            //echo "ยินดีต้อนรับคุณ MEMBER <br>";
+            //echo "<a href=index.php>กลับไปหน้าหลัก</a>";
         }
         else
         {
-            echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง<br>";
-            echo "<a href=index.php>กลับไปหน้าหลัก</a>";
+            $_SESSION['error']='error';
+            header("location:login.php");
+            die();
+            //echo "ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง<br>";
+            //echo "<a href=login.php>กลับไปหน้าหลัก</a>";
         }
        ?>
 
