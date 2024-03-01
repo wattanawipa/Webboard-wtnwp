d454f8d66a9632329719a06b4e9e70f473269c68
wattanawipa <minn7714@gmail.com>
2024-03-01 11:29:06 +0700

    Update newpost.php
    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -10,25 +10,58 @@ if(!isset($_SESSION['id'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap-5.3.2-dist/css/bootstrap.min.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>newpost</title>
 </head>
 <body>
-    <h1 style="text-align: center;">Webboard wtnwp</h1>
-    <hr>
-    <form>
-        <table>
-            <tr><td>ผู้ใช้:</td><td><?php echo $_SESSION['username'] ?></td> </tr>
-            <tr><td>หมวดหมู่:</td><td>
-                <select name "category">
-         <option value="general">เรื่องทั่วไป</option>
-         <option value="study">เรื่องเรียน</option>
-        </select>
-    </td></tr>
-            <tr><td>หัวข้อ:</td><td><input type="text"></td></tr>
-            <tr><td>เนื้อหา:</td><td><textarea name="message" rows="2" cols="30"></textarea></td></tr>
-            <tr><td colspan="2" align="center"><input type="submit" value="บันทึกข้อความ"></td></tr>
-        </table>
-    </form>
- 
+<div class="container">
+        <h1 style="text-align: center;" class="mt-3">Webboard wtnwp</h1>
+        <?php include "nav.php" ?>
+        <div class="row mt-4">
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+            <div class="col-lg-6 col-md-8 col-sm-10">
+                <div class="card border-info">
+                    <div class="card-header bg-info text-white">ตั้งกระทู้ใหม่</div>
+                    <div class="card-body">
+                        <form action="newpost_save.php" method="post">
+                            <div class="row">
+                                <label class="col-lg-3 col-form-label">หมวดหมู่ :</label>
+                                <div class="col-lg-9">
+                                    <select name="category" class="form-select">
+                                    <option value="general">เรื่องทั่วไป</option>
+                                    <option value="study">เรื่องเรียน</option>
+                                    <option value="sport">เรื่องกีฬา</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <label class="col-lg-3 col-form-label">หัวข้อ :</label>
+                                <div class="col-lg-9">
+                                    <input type="password" name="pwd" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <label class="col-lg-3 col-form-label">เนื้อหา :</label>
+                                <div class="col-lg-9">
+                                    <textarea name="comment" rows="8" class="form-control" required></textarea>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-lg-12 d-flex justify-content-center">
+                                    <button type="submit" class="btn btn-info btn-sm text-white me-2">
+                                        <i class="bi bi-caret-right-square"></i> บันทึกข้อความ</button>
+                                    <button type="reset" class="btn btn-danger btn-sm">
+                                        <i class="bi bi-x-square"></i> ยกเลิก</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+        </div>
+   </div><br>
 </body>
 </html>
