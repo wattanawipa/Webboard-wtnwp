e27f6baf5b3feb5e35ef4b8a88b8dfe2517c9417
wattanawipa <minn7714@gmail.com>
2024-03-01 10:59:52 +0700

    finish work 8
    finish work 8

--- register_save.php
+++ register_save.php
@@ -1,14 +1,30 @@
 <?php
+if(isset($_POST['login']))
+{
 $login=$_POST['login'];
-$passwd=$_POST['password'];
+$passwd=sha1($_POST['password']);
 $name=$_POST['name'];
 $gender=$_POST['gender'];
 $email=$_POST['email'];
 
 $conn=new PDO("mysql:host=localhost;dbname=webboard;charset=utf8","root","");
-
-$sql="INSERT INTO user (login, password, name, gender, email. role)
+$sql="SELECT * FROM user where login='$login'";
+$result=$conn->query($sql);
+if($result ->rowCount()==1)
+{
+    $_SESSION['add_login']='error';
+}
+else{
+$sql="INSERT INTO user (login, password, name, gender, email, role)
  VALUES ('$login','$passwd','$name','$gender','$email','m')";
 $conn->exec($sql);
+$_SESSION['add_login']="success";
+}
 $conn=null;
+header("location:login.php");
+    die();
+}else{
+    header("location:index.php");
+    die();
+}
 ?>
